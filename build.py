e48d8ae4249af5c947b5bd2ef6bd4ef797b1d5f8
keqing996 <epigrf@outlook.com>
2023-11-29 11:07:31 +0800

    Fix bugs and add some comments

--- build.py
+++ build.py
@@ -1,34 +1,67 @@
 import logging
 import subprocess
 
-# All user configuration para
+'''
+All user configuration para
+'''
+
+# cmake.exe path
 cmake_path: str = 'D:/Programmes/CMake/bin/cmake.exe'
-ndk_base_path: str = 'D:/Programmes/Android/ndk/android-ndk-r23c/'
-system_version: str = '33'
+# ndk base path
+ndk_home: str = 'D:/Programmes/Android/ndk/android-ndk-r23c/'
+# android target sdk version
+system_version: str = '31'
+# build abi
 build_abi: str = 'arm64-v8a'
+# build type
 build_type: str = 'Debug'
+# print cmake build detail
+do_verbose_output: bool = False
 
-# All needed configuration
-ndk_cmake_toolchain: str = ndk_base_path + 'build/cmake/android.toolchain.cmake'
-make_exe: str = ndk_base_path + 'prebuilt/windows-x86_64/bin/make.exe'
-c_compiler: str = ndk_base_path + 'toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe'
-cpp_compiler: str = ndk_base_path + 'toolchains/llvm/prebuilt/windows-x86_64/bin/clang++.exe'
+'''
+All needed configuration
+'''
 
-# All cmake macro args
+# ndk cmake tool chain path
+ndk_cmake_toolchain: str = ndk_home + 'build/cmake/android.toolchain.cmake'
+# ndk make.exe path
+make_exe: str = ndk_home + 'prebuilt/windows-x86_64/bin/make.exe'
+# ndk clang c compiler path
+c_compiler: str = ndk_home + 'toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe'
+# ndk clang c++ compiler path
+cpp_compiler: str = ndk_home + 'toolchains/llvm/prebuilt/windows-x86_64/bin/clang++.exe'
+
+'''
+Cmake macro args
+'''
+
+# CMAKE_BUILD_TYPE - debug or release or somethine
 arg_define_build_type: str = '-DCMAKE_BUILD_TYPE={}'.format(build_type)
+# CMAKE_MAKE_PROGRAM - where ndk make.exe located
 arg_define_make_exe: str = '-DCMAKE_MAKE_PROGRAM="{}"'.format(make_exe)
+# CMAKE_C_COMPILER - where c compiler located
 arg_define_c_compiler: str = '-DCMAKE_C_COMPILER="{}"'.format(c_compiler)
+# CMAKE_CXX_COMPILER - where c++ compiler located
 arg_define_cpp_compiler: str = '-DCMAKE_CXX_COMPILER="{}"'.format(cpp_compiler)
-arg_define_ndk: str = '-DCMAKE_ANDROID_NDK="{}"'.format(ndk_base_path)
+# CMAKE_ANDROID_NDK - where android ndk located
+arg_define_ndk: str = '-DCMAKE_ANDROID_NDK="{}"'.format(ndk_home)
+# CMAKE_TOOLCHAIN_FILE - where android ndk cmake toolchain located
 arg_define_toolchain: str = '-DCMAKE_TOOLCHAIN_FILE="{}"'.format(ndk_cmake_toolchain)
-arg_define_toolchain_ver: str = '-DCMAKE_SYSTEM_NAME="clang"'
-arg_define_system: str = '-DCMAKE_SYSTEM_NAME="Android"'
-arg_define_abi:str = '-DANDROID_ABI="{}"'.format(build_abi)
-arg_define_system_ver:str = '-DCMAKE_SYSTEM_VERSION={}'.format(system_version)
+# ANDROID_ABI - what target abi is
+arg_define_abi: str = '-DANDROID_ABI="{}"'.format(build_abi)
+# ANDROID_PLATFORM - what android sdk version is
+arg_define_android_plat: str = '-DANDROID_PLATFORM={}'.format(system_version)
+# DCMAKE_VERBOSE_MAKEFILE:BOOL - output build detail
+arg_define_verbose_output: str = '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON'
 
-# Other generate cmake args
+'''
+Other generate cmake args
+'''
+# build system
 arg_generator: str = '-G "MinGW Makefiles"'
+# source path
 arg_source_path: str = '-S "./"'
+# build output dir
 arg_build_dir: str = '-B "./build/"'
 
 def run(args_list: list[str]) -> None:
@@ -55,13 +88,15 @@ def cmake_generate() -> None:
     args_list.append(arg_define_cpp_compiler)
     args_list.append(arg_define_ndk)
     args_list.append(arg_define_toolchain)
-    args_list.append(arg_define_toolchain_ver)
-    args_list.append(arg_define_system)
     args_list.append(arg_define_abi)
-    args_list.append(arg_define_system_ver)
+    args_list.append(arg_define_android_plat)
     args_list.append(arg_generator)
     args_list.append(arg_source_path)
     args_list.append(arg_build_dir)
+
+    if do_verbose_output:
+        args_list.append(arg_define_verbose_output)
+
     run(args_list)
     pass
     
